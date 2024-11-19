2d6af8a28f4a3a7319d8ab0ec62cb31ea1413421
Tommy Nguyen <4123478+tido64@users.noreply.github.com>
2024-11-19 13:22:24 +0100

    feat: add Windows support

--- example/react-native.config.js
+++ example/react-native.config.js
@@ -1,10 +1,24 @@
 // const path = require('path');
 // const pak = require('../package.json');
 
+const { configureProjects } = require("react-native-test-app");
+
 module.exports = {
   dependencies: {
     // [pak.name]: {
     //   root: path.join(__dirname, '..'),
     // },
   },
+  project: configureProjects({
+    android: {
+      sourceDir: "android",
+    },
+    ios: {
+      sourceDir: "ios",
+    },
+    windows: {
+      sourceDir: "windows",
+      solutionFile: "windows/NitroExample.sln",
+    },
+  }),
 }
